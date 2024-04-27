1d1b2612a070b47fd9983b2d93555beb75c67c68
Karthik77991 <162857245+Karthik77991@users.noreply.github.com>
2024-04-27 08:31:16 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+const home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="Hotel KLU"
+          subtitle="Best Rooms available at low prices"
+        >
+          <Link to="/cuslogin" className="btn-home-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
+    </>
+  );
+};
+
+export default home;
